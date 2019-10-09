a4d76ab97bb0441f33d26a4cc37fe719e8302fda
Karl Lockbaum <jrlockbaum@sbcglobal.net>
2019-10-08 19:28:41 -0700

    New staging in progress

--- res-app.js
+++ res-app.js
@@ -10,6 +10,22 @@ var PORT = 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "home.html"));
+});
+
+app.get("/reservations", function(req, res) {
+  res.sendFile(path.join(__dirname, "reservations.html"));
+});
+app.get("/tables", function(req, res){
+  res.sendFile(path.join(__dirname, "/tables.html"));
+});
+
+// Displays all characters
+// app.get("/api/characters", function(req, res) {
+//   return res.json(characters);
+// });
+
 app.listen(PORT, function() {
   console.log("App listening on PORT " + PORT);
 });
